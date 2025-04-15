20a6ba8b0d9121dff46ad9667a696afc6db8ff57
sonikblue <sonikblue@users.noreply.github.com>
2025-04-15 20:59:06 +0100

    Add TODO comment

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,5 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
+// TODO: Have it build the app in "test mode", with a fixed (file?) menu
+// TODO: Assert against actual menu items
 describe("My First Test", () => {
   it("visits the app root url", () => {
     cy.visit("/");
